7c66027c8e489a3705f8c9844da051b57e0e6a07
Dimitar-Totov <dimitar.totov@abv.bg>
2025-01-31 18:07:02 +0200

    Set auth cookie

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -21,12 +21,12 @@ router.post('/login',async (req,res) => {
     const {email,password} = req.body;
     try{
         const token = await authService.login(email,password);
-        console.log(token);
+        res.cookie('auth',token);
+        res.redirect('/');
     }catch(err){
         console.log(err.message);
-        return res.redirect('/404')
+        res.redirect('/404')
     }
-    res.redirect('/');
 });
 
 
